1c475e2cf188aae1f9c93d16f5a2ea9e618be4b4
Jeya <jeyapriyagopal@gmail.com>
2018-07-01 23:09:49 -0400

    Updated Backup folder to ClosedNotes

--- WindowsFormsApplication1/ToDo.cs
+++ WindowsFormsApplication1/ToDo.cs
@@ -14,15 +14,17 @@
 //TODO: Collection By Date(may be reports)
 //TODO: Tab-Order
 //Done: Increase customer name drop down control width in create new txn screen
-//TODO: Auto Populate interest(8%)
+//Done: Auto Populate interest(8%)
+//Done: For Existing CUstomer Disable Name and Phone No.
 //TODO: For Existing CUstomer Disable Name and Phone No.
 //TODO: Please clarify - how company vs jawahar investment works?
 //TODO: While adding customer - display the customer no and sequence number eg: if 2nd time given to same customer then it should shows as 10(2nd time.)
-//TODO: Have options to show both active and non active customers in customer screen.
+//Done: Have options to show both active and non active customers in customer screen.
 //TODO: Enable sorting, paging for customer page.
 //TODO: change icon of the project.
 //TODO: installer for the project.
-//TODO: change backup folder name as [ClosedTransactions]
+//TODO: change backup folder name as [ClosedNotes]
+//TODO: Add Customer Name in ClosedNotes Folders [ClosedNotes]
 //TODO: How to see closed Transaction?
 //TODO: Sometimes file gets locked (Cant access file in use)
 //TODO: Refresh Data Automatically.
