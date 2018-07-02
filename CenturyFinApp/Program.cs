f839dd8f2f847b402773f20dbae13e8113f9f804
Subramanian, Jawahar <Jawahar.Subramanian@verisk.com>
2018-07-02 11:19:30 -0400

    Update source folder path.

--- CenturyFinApp/Program.cs
+++ CenturyFinApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Configuration;
 using System.Globalization;
+using System.IO;
+using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -23,8 +26,36 @@ namespace WindowsFormsApplication1
 
             Thread.CurrentThread.CurrentCulture = myCI;
 
+            string exeFile = (new System.Uri(Assembly.GetEntryAssembly().CodeBase)).AbsolutePath;
+            string exeDir = Path.GetDirectoryName(exeFile);
+            string dataFolder = exeDir.Replace("CenturyFinApp\\bin\\Debug", "DataAccess\\Data\\");
+
+            AddOrUpdateAppSettings("SourceFolder", dataFolder);
 
             Application.Run(new frmIndexForm());
+        }
+
+        public static void AddOrUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
         }
 
 
