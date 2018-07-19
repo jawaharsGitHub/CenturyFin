607d26ab9f86a487336cb8ad8e625789adf96336
Subramanian, Jawahar <Jawahar.Subramanian@verisk.com>
2018-07-19 14:21:01 -0400

    Rearranged TODO. Updated daily collection.

--- CenturyFinCorpApp/ToDo.cs
+++ CenturyFinCorpApp/ToDo.cs
@@ -1,31 +1,35 @@
-﻿//TODO: Multi Select Date Control
-//TODO: Tool - Auto Fill(all entries)
-//Done: Auto Select Next Transaction Date and field.
+﻿//Done: Auto Select Next Transaction Date and field.
 //Done: Auto Populate - Daily Collection Amount
 //TODO: Mobile Message Service
 //Done: Number of days taken to close the transaction.
 //Done: Actual Interest got from customer - eg: customer got - 10,000 and got all money from 100 days.then interest got is 100% that is 8% for 100 days.but if customer return all amount in 10 days.
 //Done: Start txn date should not be the first collection date, it should be the date the money given
 //Done: Update customer creation date
-//TODO: Best Customer (Top 3)
-//TODO: Worst Customers.(Top 10)
-//TODO: While Creating a customer(Existing customer) should warn for the worst customer.
-//TODO: Expected Amount the given date(Future Date)
 //Done: Collection By Date(may be reports)
-//TODO: Tab-Order
 //Done: Increase customer name drop down control width in create new txn screen
 //Done: Auto Populate interest(8%)
 //Done: For Existing CUstomer Disable Name and Phone No.
 //Done: For Existing CUstomer Disable Name and Phone No.
 //Done: (Removed this feature) Please clarify - how company vs jawahar investment works?
-//TODO: While adding customer - display the customer no and sequence number eg: if 2nd time given to same customer then it should shows as 10(2nd time.)
 //Done: Have options to show both active and non active customers in customer screen.
+//Done: change backup folder name as [ClosedNotes]
+//Done: How to see closed Transaction?
+// Done: Asc/Desc by date
+
+
+
+//TODO: Multi Select Date Control
+//TODO: Tool - Auto Fill(all entries)
+//TODO: Best Customer (Top 3)
+//TODO: Worst Customers.(Top 10)
+//TODO: While Creating a customer(Existing customer) should warn for the worst customer.
+//TODO: Expected Amount the given date(Future Date)
+//TODO: Tab-Order
+//TODO: While adding customer - display the customer no and sequence number eg: if 2nd time given to same customer then it should shows as 10(2nd time.)
 //TODO: Enable sorting, paging for customer page.
 //TODO: change icon of the project.
 //TODO: installer for the project.
-//Done: change backup folder name as [ClosedNotes]
 //TODO: Add Customer Name in ClosedNotes Folders [ClosedNotes]
-//Done: How to see closed Transaction?
 //TODO: Sometimes file gets locked (Cant access file in use)
 //TODO: Refresh Data Automatically.
 //TODO: TxnId may not be correct if last customer closes
@@ -40,6 +44,8 @@
 // TODO: Delete customer
 //TODO: Git Flow for all customers.
 //TODO: Set focus to txtSearch in customer form.
-// Done: Asc/Desc by date
+
 // TODO: Esc should close the form.
 //Issue: Check customer and total notes count.
+// TODO: list who not payed for the given date.
+// TODO: AUTO - Collection sheet.
