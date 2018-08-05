5ac5df6b754b94dc6d6b10df2d7b1c6b9bb2e259
Jeya <jeyapriyagopal@gmail.com>
2018-08-05 00:45:49 -0400

    updated form width

--- CenturyFinCorpApp/frmCustomerTransaction.Designer.cs
+++ CenturyFinCorpApp/frmCustomerTransaction.Designer.cs
@@ -141,7 +141,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(18, 183);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(714, 307);
+            this.dataGridView1.Size = new System.Drawing.Size(467, 307);
             this.dataGridView1.TabIndex = 6;
             this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
             // 
