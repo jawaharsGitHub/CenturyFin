c74b124dea2258c53b9d5f8caed2f38e740d0713
Jeya <jeyapriyagopal@gmail.com>
2018-08-14 23:14:15 -0400

    daily cxn 14 aug 2018. updated vasu and vignesh txns.

--- DataAccess/PrimaryTypes/Transaction.cs
+++ DataAccess/PrimaryTypes/Transaction.cs
@@ -429,14 +429,15 @@ namespace DataAccess.PrimaryTypes
                         join t in result on c.CustomerSeqNumber equals t.CustomerSequenceNo
                         select new
                         {
-                            c.CustomerSeqNumber,
+                            
                             c.Name,
+                            RunningDays = (DateTime.Now - c.AmountGivenDate).Value.Days,
+                            DaysToClose = ((DateTime.Now - c.AmountGivenDate.Value).TotalDays) > 100 ? (100 - (DateTime.Now - c.AmountGivenDate.Value.Date).Days) : (t.Balance / (c.LoanAmount / 100)),
+                            NeedToClose = ((DateTime.Now - c.AmountGivenDate.Value).TotalDays) > 100 ? 0 : ((c.AmountGivenDate.Value.AddDays(101) - DateTime.Today.Date).Days),
                             c.AmountGivenDate,
                             c.LoanAmount,
                             t.Balance,
-                            RunningDays = (DateTime.Now - c.AmountGivenDate).Value.Days,
-                            DaysToClose = ((DateTime.Now - c.AmountGivenDate.Value).TotalDays) > 100 ? (100 - (DateTime.Now - c.AmountGivenDate.Value.Date).Days) : (t.Balance / (c.LoanAmount / 100)),
-                            NeedToClose = ((DateTime.Now - c.AmountGivenDate.Value).TotalDays) > 100 ? 0 : ((c.AmountGivenDate.Value.AddDays(101) - DateTime.Today.Date).Days)
+                            c.CustomerSeqNumber,
                         }).OrderBy(o => o.NeedToClose).Take(top).ToList();
 
             return data;
