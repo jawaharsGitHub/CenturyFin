31da35e49e7ceeccc57670668d8b564469b92aa3
Jeya <jeyapriyagopal@gmail.com>
2018-08-28 01:45:28 -0400

    found out discripinicies txns.

--- CenturyFinCorpApp/Program.cs
+++ CenturyFinCorpApp/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Common;
+using DataAccess.PrimaryTypes;
 using System;
 using System.Globalization;
 using System.Threading;
 using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace CenturyFinCorpApp
 {
@@ -29,6 +33,37 @@ namespace CenturyFinCorpApp
             if (AppConfiguration.AddOrUpdateAppSettings("SourceFolder", dataFolder))
             {
                 LogHelper.WriteLog($"started application");
+
+                // As of 27-1ug-2018
+                /*
+                var details = (from c in Customer.GetAllCustomer()
+                               select new {
+                                   c.CustomerId,
+                                   c.CustomerSeqNumber,
+                                   c.Name ,
+                                   c.LoanAmount,
+                                   txn = Transaction.GetTransactionDetails(c.CustomerId, c.CustomerSeqNumber, (c.IsActive == false)) }).ToList();
+
+                var data = (from t in details
+                            select new {
+                                t.CustomerId,
+                                t.CustomerSeqNumber,
+                                t.Name,
+                                t.LoanAmount,
+                                TotalReceived = t.txn.Sum(s => s.AmountReceived),
+                                MinBalance = t.txn.Min(m => m.Balance),
+                                IsCorrect =  ((t.LoanAmount - t.txn.Sum(s => s.AmountReceived)) == t.txn.Min(m => m.Balance))
+                            }
+                            ).ToList();
+
+                StringBuilder sb = new StringBuilder();
+
+                string jsonString = JsonConvert.SerializeObject(data, Formatting.Indented);
+                */
+
+
+
+
                 Application.Run(new frmIndexForm());
                 
             }
