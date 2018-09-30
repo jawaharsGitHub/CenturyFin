09b52eb7243b8f525cf73f6ec7b292bafba8689b
Jeya <jeyapriyagopal@gmail.com>
2018-09-29 22:39:57 -0400

    final petrol report is 35KM per litre.

--- CenturyFinCorpApp/UsrCtrl/frmPetrol.cs
+++ CenturyFinCorpApp/UsrCtrl/frmPetrol.cs
@@ -5,6 +5,13 @@ using System;
 using System.Linq;
 using System.Windows.Forms;
 
+
+/*
+ 	Actual	50KM	40KM	35KM
+Rs	1700	1740	1740	1740
+KM	700 	1000	800 	700
+
+ */
 namespace CenturyFinCorpApp.UsrCtrl
 {
     public partial class frmPetrol : UserControl
@@ -27,8 +34,14 @@ namespace CenturyFinCorpApp.UsrCtrl
 
             var runningDays = (lastDate.Date - firstDate.Date).TotalDays + 1;
 
-            lblAvgKm.Text = $"Average Km PerDay : {(totalRunKm/runningDays).ToInt32()} between {firstDate.Date.ToShortDateString()} and {lastDate.Date.ToShortDateString()} {Environment.NewLine}" +
-                $"Running Kms : {totalRunKm} RunningDays : {runningDays}";
+            var avgPrice = data.Where(w => w.Date >= firstDate.Date).Average(a => a.Amount);
+            var fullPrice = data.Where(w => w.Date >= firstDate.Date).Sum(a => a.Amount);
+
+
+            lblAvgKm.Text = $"Average Km PerDay : {(totalRunKm / runningDays).ToInt32()} between {firstDate.Date.ToShortDateString()} and {lastDate.Date.ToShortDateString()} {Environment.NewLine}" +
+                $"Running Kms : {totalRunKm} RunningDays : {runningDays}{Environment.NewLine}" +
+                $"Amount Price Per Day : {avgPrice}{Environment.NewLine}" +
+                $"Full Amount : {fullPrice}";
 
 
         }
