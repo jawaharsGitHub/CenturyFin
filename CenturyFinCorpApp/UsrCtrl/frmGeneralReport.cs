934865f4e089b29aafbd2377425f20b1708e6968
Jeya <jeyapriyagopal@gmail.com>
2018-11-18 12:15:16 -0500

    updated inv.

--- CenturyFinCorpApp/UsrCtrl/frmGeneralReport.cs
+++ CenturyFinCorpApp/UsrCtrl/frmGeneralReport.cs
@@ -268,7 +268,7 @@ namespace CenturyFinCorpApp.UsrCtrl
         {
             var inHandAndBank = InHandAndBank.GetAllhandMoney();
             lblTotalAsset.Text = $"{(outstandingMoney.includesProfit + inHandAndBank.InHandAmount + inHandAndBank.InBank).ToMoney()} (OS: {outstandingMoney.includesProfit.ToMoney()} IH: {inHandAndBank.InHandAmount.ToMoney()} IB: {inHandAndBank.InBank.ToMoney()} Actual Outstanding: {outstandingMoney.actual.ToMoney()})";
-            lblBizAsset.Text = $"{(outstandingMoney.includesProfit + inHandAndBank.InHandAmount).ToMoney()} (OS: {outstandingMoney.includesProfit.ToMoney()} IH: {inHandAndBank.InHandAmount.ToMoney()}) out of ~16.5-Lacs as of Nov 16 2018.";
+            lblBizAsset.Text = $"{(outstandingMoney.includesProfit + inHandAndBank.InHandAmount).ToMoney()} (OS: {outstandingMoney.includesProfit.ToMoney()} IH: {inHandAndBank.InHandAmount.ToMoney()}) out of ~16.75-Lacs as of Nov 16 2018.";
         }
     }
 }
