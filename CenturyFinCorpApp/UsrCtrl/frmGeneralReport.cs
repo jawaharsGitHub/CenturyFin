fba6055801c6dabf4ea6ec1c5ffdc84196c923e4
Jeya <jeyapriyagopal@gmail.com>
2019-02-28 05:28:43 -0500

    topup + few minor changes.

--- CenturyFinCorpApp/UsrCtrl/frmGeneralReport.cs
+++ CenturyFinCorpApp/UsrCtrl/frmGeneralReport.cs
@@ -285,7 +285,12 @@ namespace CenturyFinCorpApp.UsrCtrl
             var inHandAndBank = InHandAndBank.GetAllhandMoney();
             lblTotalAsset.Visible = label4.Visible = false;
             lblTotalAsset.Text = $"{(outstandingMoney.includesProfit + inHandAndBank.InHandAmount + inHandAndBank.InBank).ToMoney()} (OS: {outstandingMoney.includesProfit.ToMoney()} IH: {inHandAndBank.InHandAmount.ToMoney()} IB: {inHandAndBank.InBank.ToMoney()} Actual Outstanding: {outstandingMoney.actual.ToMoney()})";
-            lblBizAsset.Text = $"{(outstandingMoney.includesProfit + inHandAndBank.InHandAmount).ToMoney()} (OS: {outstandingMoney.includesProfit.ToMoney()} IH: {inHandAndBank.InHandAmount.ToMoney()}  {Environment.NewLine} Actual Outstanding: {outstandingMoney.actual.ToMoney()}) out of ~17.20-Lacs as of Nov 16 2018. + (2 + 1 = 3) L monthly [{DailyCollectionDetail.GetActualInvestmentTxnDate().ToMoney()}]";
+            var monthlyCustomersInv = Customer.GetAllCustomer().Where(w => w.IsActive && w.ReturnType == DataAccess.ExtendedTypes.ReturnTypeEnum.Monthly).Sum(s => s.LoanAmount);
+            var fullInvestment = DailyCollectionDetail.GetActualInvestmentTxnDate();
+
+            lblBizAsset.Text = $"{(outstandingMoney.includesProfit + inHandAndBank.InHandAmount).ToMoney()} " +
+                $"(OS: {outstandingMoney.includesProfit.ToMoney()} + IH: {inHandAndBank.InHandAmount.ToMoney()})  {Environment.NewLine} " +
+                $"Actual Outstanding: {outstandingMoney.actual.ToMoney()} {Environment.NewLine} INVESTMENT: Daily:~{(fullInvestment - monthlyCustomersInv).ToMoney()} + Monthly:{monthlyCustomersInv.ToMoney()} = {fullInvestment.ToMoney()}";
         }
 
         private void dgvIncome_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
