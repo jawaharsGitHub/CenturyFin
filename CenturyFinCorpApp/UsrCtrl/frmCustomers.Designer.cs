97cbdb4875a61143d9241bf05f0c25d54efebb0b
Jeya <jeyapriyagopal@gmail.com>
2019-03-01 13:22:08 -0500

    minor changes.

--- CenturyFinCorpApp/UsrCtrl/frmCustomers.Designer.cs
+++ CenturyFinCorpApp/UsrCtrl/frmCustomers.Designer.cs
@@ -123,7 +123,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(993, 0);
+            this.label1.Location = new System.Drawing.Point(947, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 10;
