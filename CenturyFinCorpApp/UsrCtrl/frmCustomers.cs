11a2226021d2ea66770f6b848653799187869725
Jeya <jeyapriyagopal@gmail.com>
2019-10-17 12:42:00 +0530

    updates

--- CenturyFinCorpApp/UsrCtrl/frmCustomers.cs
+++ CenturyFinCorpApp/UsrCtrl/frmCustomers.cs
@@ -30,7 +30,8 @@ namespace CenturyFinCorpApp
        NI = 8,
        None = 9
        Loss = 10
-       Random = 11";
+       Random = 11
+       Others = 12";
 
             cmbFilters.DataSource = GetOptions();
             SetReturnTypes();
@@ -169,19 +170,14 @@ namespace CenturyFinCorpApp
               $"  need {365 - totalTxn} in {365 - days} days [Shortage: {days - totalTxn}] {Environment.NewLine} " +
               $"{DateHelper.DaysToMonth(" Days Left", DateTime.Today, new DateTime(2019, 1, 24))}";
 
+            
 
-            //label1.Text = $"{totalTxn} notes in {days} days {Environment.NewLine} " +
-            //    $"{DateHelper.DaysToMonth("Running Days", new DateTime(2018, 1, 25), DateTime.Today)} {Environment.NewLine} " +
-            //    $"{Math.Round(totalTxn / days, 2)} note(s) per day {Environment.NewLine} " +
-            //    $"{Math.Round(closedTxn / days, 2)} closed note(s) per day {Environment.NewLine} " +
-            //    $"{closedMonthlyCustomers.Sum(s => s.Interest).ToMoney()}(C) [{closedMonthlyCustomers.Sum(s => s.LoanAmount).ToMoney()}] + {activeMonthlyCustomers.Sum(s => s.Interest).ToMoney()}(A) [{activeMonthlyCustomers.Sum(s => s.Balance).ToMoney()}] = {monthlyCustomers.Sum(s => s.Interest).ToMoney()} out of ({monthlyCustomers.Sum(s => s.LoanAmount).ToMoney()}).{Environment.NewLine} " +
-            //    $"{Math.Round(allGivenAmount / days).TokFormat()} Rs. per day ({((Math.Round(allGivenAmount / days) / 10) * 30).TokFormat()} per month) {Environment.NewLine}" +
-            //    $"  need {365 - totalTxn} in {365 - days} days [Shortage: {days - totalTxn}] {Environment.NewLine} " +
-            //    $"{DateHelper.DaysToMonth(" Days Left", DateTime.Today, new DateTime(2019, 1, 24))}";
         }
 
         private void AdjustColumnOrder()
         {
+            dataGridView1.Columns["MonthlyInterest"].HeaderText = "Monthly Payment";
+
             dataGridView1.Columns["CollectionAmt"].DisplayIndex = 4;
             dataGridView1.Columns["ReturnDay"].DisplayIndex = 4;
             dataGridView1.Columns["ReturnType"].DisplayIndex = 5;
--- DataAccess/ExtendedTypes/ReturnTypeEnum.cs
+++ DataAccess/ExtendedTypes/ReturnTypeEnum.cs
@@ -14,6 +14,7 @@
         NI,
         None,
         Loss,
-        Random
+        Random,
+        Others
     }
 }
