e1c4abfdcf8cf5fbbadd07380b69e76f08a1ddb4
jawahar <jawahars@live.in>
2020-03-21 19:59:17 +0530

    cleaning.

--- Common/AppConfiguration.cs
+++ Common/AppConfiguration.cs
@@ -13,11 +13,11 @@ namespace Common
 
         public static string TransactionFile { get; } = GetFullPath("TransactionFile");
 
-        public static string InvestmentFile { get; } = GetFullPath("InvestmentFile");
+        //public static string InvestmentFile { get; } = GetFullPath("InvestmentFile");
 
         public static string ClosedNotesFile { get; } = GetFullPath("ClosedNotesFile");
 
-        public static string ExpenditureFile { get; } = GetFullPath("ExpenditureFile");
+        //public static string ExpenditureFile { get; } = GetFullPath("ExpenditureFile");
 
         public static string InHandFile { get; } = GetFullPath("InHandAndBankFile");
 
@@ -25,7 +25,7 @@ namespace Common
 
         public static bool usingMenu { get; } = Convert.ToBoolean(ConfigurationManager.AppSettings["usingMenu"]);
 
-        public static string CollectionPerDay { get; } = GetFullPath("CollectionPerDay");
+        //public static string CollectionPerDay { get; } = GetFullPath("CollectionPerDay");
 
         public static string DailyBatchFile { get; } = GetFullPath("DailyBatchFile");
 
