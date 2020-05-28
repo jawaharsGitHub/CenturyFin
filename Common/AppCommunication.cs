7501d4a454d054d805b174d203620eb3982c748f
jawahar <jawahars@live.in>
2020-05-28 19:35:37 +0530

    updates

--- Common/AppCommunication.cs
+++ Common/AppCommunication.cs
@@ -7,29 +7,35 @@ namespace Common
 {
     public class AppCommunication
     {
+        private static (MailMessage, SmtpClient) GetMailMessage(string subject, string mailBody)
+        {
 
+            var myEmail = "jawahar.subramanian83@gmail.com";
+            MailMessage message = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+            message.From = new MailAddress(myEmail);
+            message.To.Add(new MailAddress(myEmail));
+            message.Subject = subject;
+            message.IsBodyHtml = true; //to make message body as html  
+            message.Body = mailBody;
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com"; //for gmail host  
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential(myEmail, "nainamarbus");
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            return (message, smtp);
+
+
+        }
         public static void SendBalanceEmail(string mailBody, DateTime collectionDate, string activeCusCount, string subject)
         {
             try
             {
-
-                var myEmail = "jawahar.subramanian83@gmail.com";
-                MailMessage message = new MailMessage();
-                SmtpClient smtp = new SmtpClient();
-                message.From = new MailAddress(myEmail);
-                message.To.Add(new MailAddress(myEmail));
-                message.Subject = $"[{activeCusCount}] {subject} - {collectionDate.Ddmmyy()}";
-                message.IsBodyHtml = true; //to make message body as html  
-                message.Body = mailBody;
-                smtp.Port = 587;
-                smtp.Host = "smtp.gmail.com"; //for gmail host  
-                smtp.EnableSsl = true;
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential(myEmail, "nainamarbus");
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-
-
-                smtp.Send(message);
+                var sub = $"[{activeCusCount}] {subject} - {collectionDate.Ddmmyy()}"; ;
+                var smtp = GetMailMessage(sub, mailBody);
+                smtp.Item2.Send(smtp.Item1);
             }
             catch (Exception ex)
             {
@@ -43,55 +49,25 @@ namespace Common
             {
                 if (emailStructure == null) return;
 
-                var myEmail = "jawahar.subramanian83@gmail.com";
-                MailMessage message = new MailMessage();
-                SmtpClient smtp = new SmtpClient();
-                message.From = new MailAddress(myEmail);
-                message.To.Add(new MailAddress(myEmail));
-                message.Subject = $"{emailStructure.CollectionDate.Ddmmyy()} {emailStructure.Subject} Report";
-                message.IsBodyHtml = true; //to make message body as html  
-                message.Body = emailStructure.HtmlContent;
-                smtp.Port = 587;
-                smtp.Host = "smtp.gmail.com"; //for gmail host  
-                smtp.EnableSsl = true;
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential(myEmail, "nainamarbus");
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-
-
-                smtp.Send(message);
+                var sub = $"{emailStructure.CollectionDate.Ddmmyy()} {emailStructure.Subject} Report";
+                var smtp = GetMailMessage(sub, emailStructure.HtmlContent);
+                smtp.Item2.Send(smtp.Item1);
+             
             }
             catch (Exception ex)
             {
                 throw ex;
             }
         }
-
 
         public static void SendCustomerTxnEmail(string mailBody, DateTime collectionDate, string attachmentFilePath)
         {
             try
             {
-
-                var myEmail = "jawahar.subramanian83@gmail.com";
-                MailMessage message = new MailMessage();
-                SmtpClient smtp = new SmtpClient();
-                message.From = new MailAddress(myEmail);
-                message.To.Add(new MailAddress(myEmail));
-                message.Subject = $"Transaction For {mailBody} On {collectionDate.Ddmmyy()}";
-                message.IsBodyHtml = false; //to make message body as html  
-                message.Body = mailBody;
-                smtp.Port = 587;
-                smtp.Host = "smtp.gmail.com"; //for gmail host  
-                smtp.EnableSsl = true;
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential(myEmail, "nainamarbus");
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-
-                message.Attachments.Add(new Attachment(attachmentFilePath));
-
-
-                smtp.Send(message);
+                var sub = $"Transaction For {mailBody} On {collectionDate.Ddmmyy()}";
+                var smtp = GetMailMessage(sub, mailBody);
+                smtp.Item1.Attachments.Add(new Attachment(attachmentFilePath)); // attachments
+                smtp.Item2.Send(smtp.Item1);
             }
             catch (Exception ex)
             {
@@ -99,32 +75,5 @@ namespace Common
             }
         }
 
-
-
-        public static void SendSms(string mailBody, DateTime collectionDate)
-        {
-            try
-            {
-
-
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public static void SendWhatsApp(string mailBody, DateTime collectionDate)
-        {
-            try
-            {
-
-
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
     }
 }
